845304e543b3f6118d524c5d824dc4d33aef92ff
Aditya Marzuk <adityamms86@gmail.com>
2023-09-16 18:15:05 +0700

    fix output err
    fix output err

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  target: "experimental-serverless-trace",
+  output: "export",
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
