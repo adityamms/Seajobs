3c82598eff853029457e9e9f133c8ca010b70481
Aditya Marzuk <adityamms86@gmail.com>
2023-09-29 12:24:55 +0700

    cek

--- functions/hello.js
+++ functions/hello.js
@@ -3,52 +3,58 @@ import seaman from "@/model/seaman.js";
 import { connectToDB } from "@/utils/conectDb";
 
 exports.handler = async (event, context) => {
+  let sendDate = new Date().getTime();
   await connectToDB();
+
   if (event.httpMethod === "GET") {
-    const limit = 10; // You can specify the limit as a query parameter
+    try {
+      const limit = 10; // You can specify the limit as a query parameter
 
-    // Set a default limit or use the value from the query parameter
-    const itemsLimit = parseInt(limit) || 10;
+      // Set a default limit or use the value from the query parameter
+      const itemsLimit = parseInt(limit) || 10;
 
-    const query = {};
+      const query = {};
 
-    const cari = await employer.find(query, { lowongan: 1 }).limit(itemsLimit);
+      const cari = await employer
+        .find(query, { lowongan: 1 })
+        .limit(itemsLimit);
 
-    if (!cari || cari.length === 0) {
-      // Handle case where no matching employers are found
+      let receiveDate = new Date().getTime();
+      let responseTimeMs = receiveDate - sendDate;
+      console.log(responseTimeMs);
       return {
-        staqtusCode: 404, // or any other HTTP status code
-        body: JSON.stringify({ message: "no matching" }),
+        statusCode: 200, // or any other HTTP status code
+        body: JSON.stringify(cari),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500, // or any other HTTP status code
+        body: JSON.stringify({ err: error }),
       };
     }
-
-    return {
-      statusCode: 200, // or any other HTTP status code
-      body: JSON.stringify(cari),
-    };
   }
 
-  // if (req.method === "POST") {
-  //   await connectToDB();
-  //   if (req.body.account_type === "Company") {
-  //     try {
-  //       let baru = await new employer(req.body);
-  //       console.log(baru);
-  //       await baru.save();
-  //       res.status(200).end();
-  //     } catch (error) {
-  //       console.log(error);
-  //       res.status(400).json({ meg: "gagal" });
-  //     }
-  //   } else
-  //     try {
-  //       let baru = await new seaman(req.body);
-  //       console.log(baru);
-  //       await baru.save();
-  //       res.status(200).end();
-  //     } catch (error) {
-  //       console.log(error.msg);
-  //       res.status(400).json({ meg: "gagal" });
-  //     }
-  // }
+  if (req.method === "POST") {
+    await connectToDB();
+    if (req.body.account_type === "Company") {
+      try {
+        let baru = await new employer(req.body);
+        console.log(baru);
+        await baru.save();
+        res.status(200).end();
+      } catch (error) {
+        console.log(error);
+        res.status(400).json({ meg: "gagal" });
+      }
+    } else
+      try {
+        let baru = await new seaman(req.body);
+        console.log(baru);
+        await baru.save();
+        res.status(200).end();
+      } catch (error) {
+        console.log(error.msg);
+        res.status(400).json({ meg: "gagal" });
+      }
+  }
 };
--- functions/testTime.js
+++ functions/testTime.js
@@ -1,8 +1,14 @@
 import { connectToDB } from "@/utils/conectDb";
 import seaman from "@/model/seaman";
 exports.handler = async (event, context) => {
+  let sendDate = new Date().getTime();
   await connectToDB();
-  let coba = seaman.find({});
+  let receiveDate = new Date().getTime();
+  let responseTimeMs = receiveDate - sendDate;
+
+  console.log(responseTimeMs);
+
+  let coba = await seaman.find({});
   return {
     statusCode: 200, // or any other HTTP status code
     body: JSON.stringify({ message: "berhasil", coba }),
